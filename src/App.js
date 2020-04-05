4085de7a72a9bf6323019485e65980be6078ca4d
Mahir <mdhall@stevens.edu>
2020-04-05 17:31:32 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,43 @@
 import React from 'react';
-import logo from './img/pika.png';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PokemonList from './components/PokemonList';
 import Pokemon from './components/Pokemon';
 import BerriesList from './components/BerriesList';
 import Error from './components/Error';
 import Berries from './components/Berries';
 import MachinesList from './components/MachinesList';
-import Machines from './components/Machines'
+import Machines from './components/Machines';
+import Home from './components/Home';
 
 
 function App() {
   return (
     <Router>
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to your pokedex</h1>
-          
-          <Link className='showlink' to='/pokemon/page/0'>Pokemon</Link>
-          <Link className='showlink' to='/berries/page/0'>Berries</Link>
-          <Link className='showlink' to='/machines/page/0'>Machines</Link>
-          
 
-        </header>
 
-        <br />
-        <br />
 
-        <div className='App-body'>
-          
-          <p>The first link is a complete list of all 890 species of Pokémon currently known to exist</p>
-          <p>The second link is a complete list of all available berries that help your Pokemon grow and improve its powers</p>
-          <p>The third link is a complete list of all available machines that you as a pokemon trainer can buy to train your Pokemon</p>
 
-          
-          <Route path='/pokemon/page/:page' exact component={PokemonList} />
-          <Route path='/berries/page/:page' exact component={BerriesList} />
-          <Route path='/machines/page/:page' exact component={MachinesList} />
-          <Route path='/pokemon/:id' exact component={Pokemon} />
-          <Route path='/berries/:id' exact component={Berries} />
-          <Route path='/machines/:id' exact component={Machines} />
-          <Route path='/notfound' exact component={Error} />
-          
-          
 
-        </div>
+
+
+
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/pokemon/page/:page' component={PokemonList} />
+          <Route exact path='/berries/page/:page' component={BerriesList} />
+          <Route exact path='/machines/page/:page' component={MachinesList} />
+          <Route exact path='/pokemon/:id' component={Pokemon} />
+          <Route exact path='/berries/:id' component={Berries} />
+          <Route exact path='/machines/:id' component={Machines} />
+          <Route exact path='/notfound' component={Error} />
+          <Route path="*" component={Error} status={404} />
+        </Switch>
+
+
+
+
 
       </div>
 
